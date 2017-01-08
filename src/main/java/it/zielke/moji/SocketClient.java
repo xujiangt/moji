d1259556f4c28b86a8b54b3fd71b54a2107c15e8
xujiangt <xujiangtt@163.com>
2017-01-08 16:35:25 +0800

    add author

--- src/main/java/it/zielke/moji/SocketClient.java
+++ src/main/java/it/zielke/moji/SocketClient.java
@@ -22,6 +22,7 @@ import org.apache.commons.io.FilenameUtils;
 /**
  * Client for communicating with the MOSS server socket. Handles the
  * configuration, sending of files and receiving results.
+ * author:jiangtao.xjt
  */
 public class SocketClient {
 	private static final String MESSAGE_UNKNOWN_LANGUAGE = "MOSS Server does not recognize this programming language";
